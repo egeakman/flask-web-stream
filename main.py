90bd4b5f64875c567e8f919c45d992110251e488
egeakman <egeakmanegeakman@hotmail.com>
2021-12-02 15:33:38 +0300

    Initial commit :)

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, Response
+import cv2
+
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+
+def gen_frames():
+    while True:
+        grabbed, frame = camera.read()
+        if grabbed == 1:
+            ret, buffer = cv2.imencode(".jpg", frame)
+            frame = buffer.tobytes()
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
